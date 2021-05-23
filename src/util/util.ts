83161e6cfa251c3dbcc694bd77e3968fc1a55727
Bruno Fernandes <ofbruno@gmail.com>
2021-05-23 20:28:18 -0300

    Endpoint creation for filtering images

--- src/util/util.ts
+++ src/util/util.ts
@@ -8,18 +8,19 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
-    });
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+	return new Promise(async (resolve) => {
+		const photo = await Jimp.read(inputURL);
+		const outpath =
+			'/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+		await photo
+			.resize(256, 256) // resize
+			.quality(60) // set JPEG quality
+			.greyscale() // set greyscale
+			.write(__dirname + outpath, (img) => {
+				resolve(__dirname + outpath);
+			});
+	});
 }
 
 // deleteLocalFiles
@@ -27,8 +28,26 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
-        fs.unlinkSync(file);
-    }
+export async function deleteLocalFiles(files: Array<string>) {
+	for (let file of files) {
+		fs.unlinkSync(file);
+	}
+}
+
+export function validURL(str: string): boolean {
+	if (!str) {
+		return false;
+	}
+
+	var pattern = new RegExp(
+		'^(https?:\\/\\/)?' + // protocol
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+			'(\\#[-a-z\\d_]*)?$',
+		'i'
+	); // fragment locator
+
+	return !!pattern.test(str);
 }
